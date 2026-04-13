d37d8f2282fd074dda0e63b0fdf41ef4c0ef4e50
losion <losion445@gmail.com>
2026-04-13 15:57:44 +0300

    [INFRASTRUCTURE] Simple bot run with docker compose

--- application/cmd/main.go
+++ application/cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	tele "gopkg.in/telebot.v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	bot, err := tele.NewBot(tele.Settings{
+		Token: os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Handle("/start", func(c tele.Context) error {
+		return c.Send("Поддержка")
+	})
+
+	bot.Start()
 }
--- application/go.mod
+++ application/go.mod
@@ -1,3 +1,5 @@
 module github.com/losion445-max/support_bot-golang
 
 go 1.26.1
+
+require gopkg.in/telebot.v4 v4.0.0-beta.7
